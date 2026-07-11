7e9e5456c994911306290d9bff8b69fa3b9876e5
YASSER Korzane <y_korzane@estin.dz>
2026-07-11 14:52:26 +0100

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.go
@@ -0,0 +1,10 @@
+func hasDuplicate(nums []int) bool {
+   m := make(map[int]struct{})
+   for _, v := range nums {
+       if _, ok := m[v]; ok {
+           return true
+       }
+       m[v] = struct{}{}
+   }
+   return false
+}
